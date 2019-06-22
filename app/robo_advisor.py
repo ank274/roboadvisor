399d90309f3e4bc13e6b8abc412e5b9648cd6670
Annie Kong <annie.kong@stern.nyu.edu>
2019-06-22 19:14:05 -0400

    CSV function added

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -16,8 +16,20 @@ elif(user_input.isdigit()):
     exit()
 data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}'.format(user_input,key))
 response = data.status_code
-parse_data = data.json()
-if ('Error Message' in data.text or response !=200):
+parsed_data = data.json()
+if ('Error Message' in parsed_data or response !=200):
      print("error not a valid stock")
      exit()
+else:
+    all_date = list(parsed_data['Time Series (Daily)'])
+    file_name = "../data/price_{}.csv".format(user_input)
+    f = open(file_name, "w")
+    headers = "date,open,high,low,close,volume\n"
+    f.write(headers)
+    for date_list in all_date:
+        f.write(date_list + "," + parsed_data['Time Series (Daily)'][date_list]["1. open"] + "," 
+        + parsed_data['Time Series (Daily)'][date_list]["2. high"] + "," + parsed_data['Time Series (Daily)'][date_list]["3. low"] 
+        + "," + parsed_data['Time Series (Daily)'][date_list]["4. close"] + "," + parsed_data['Time Series (Daily)'][date_list]["5. volume"] + "\n")
+    f.close()
+
 
