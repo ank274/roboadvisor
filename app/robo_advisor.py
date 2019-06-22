b37548c61c17f3d149200230aa5efbfaf73dc945
Annie Kong <annie.kong@stern.nyu.edu>
2019-06-22 18:50:17 -0400

    Checked for User inputs

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,4 +8,13 @@ key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MFST&apikey={}'.format(key))
 data = data.json()
-print(data)
+
+
+user_input = input("Please enter a Stock symbol")
+length = len(user_input)
+if (length <= 0 or length > 6):
+    print("inncorrect length")
+    exit()
+elif(user_input.isdigit()):
+    print("Input cannot be a digit")
+    exit()
